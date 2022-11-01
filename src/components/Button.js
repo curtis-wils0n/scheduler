054f51a25544bc9a23ce4cb7e8025f49ce2385f7
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-01 14:37:15 -0600

    Create button styles and tests with Storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = {
+    'button--confirm': props.confirm,
+    'button--danger': props.danger, 
+  };
+
+  return (
+    <button
+      disabled={props.disabled}
+      className={classNames('button', buttonClass)}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
