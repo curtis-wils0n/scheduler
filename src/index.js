b4d5452d2470b5b21714cd69df9b17c208b18281
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-03 16:06:17 -0600

    Replace hardcoded Days list with axios request to local server.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
-import Appointment from "components/Appointment";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
