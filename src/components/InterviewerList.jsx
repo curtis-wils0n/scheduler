5ec4ecbdbfd94d1d3e0da8760d21d24ab8964bff
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-08 14:21:42 -0700

    Implement type-checking with prop-type

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types'
 
 import "components/InterviewerList.scss";
 
@@ -24,3 +25,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
