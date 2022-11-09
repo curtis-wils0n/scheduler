500a450fd4d73e71b89449dd2a864abdeef0024b
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-11-08 17:30:21 -0700

    Include tests for Application appointment creation

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -76,5 +76,11 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+  put: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
   })
 }
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,18 +1,56 @@
 import React from "react";
-import axios from "__mocks__/axios";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { 
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+  prettyDOM,
+  getByText,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defauls to Monday and changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
+  it("defauls to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"));
+    
+    fireEvent.click(getByText("Tuesday"));
+    
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  it("loads data, books an interview, and reduces the spots remaining for the first day by 1", async () => {
+    const { container, debug } = render(<Application />);
+    
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    const appointment = getAllByTestId(container, "appointment")[0];
+    
+    fireEvent.click(getByAltText(appointment, "Add"));
 
-  await waitForElement(() => getByText("Monday"));
-  
-  fireEvent.click(getByText("Tuesday"));
-  
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+
+    const day = getAllByTestId(container, "day").find(day => 
+      getByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  });
+})
